bf1180b1fbb5079838c9e099a41b0f3c34165b88
Alex <muzikbasss@gmail.com>
2024-02-06 07:29:02 +0100

    feat: adoptation

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	const dropdown = document.getElementById("countryDropdown");
 	const selectedOption = dropdown.querySelector(".modal-selected-option");
 	const optionsList = dropdown.querySelector(".modal-options-list");
-	
+
 	selectedOption.addEventListener("click", function () {
 		optionsList.style.display = optionsList.style.display === "block" ? "none" : "block";
 	});
@@ -13,13 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
 			let selectedValue = e.target.getAttribute("data-value");
 			selectedOption.textContent = e.target.textContent;
 			optionsList.style.display = "none";
-			// Обновление значения телефона
 			updatePhoneNumber(selectedValue);
 		}
 	});
 
 	function updatePhoneNumber(value) {
-		// Логика обновления значения телефона (если требуется)
 		console.log("Выбран код страны: " + value);
 	}
 
@@ -28,8 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
 			optionsList.style.display = "none";
 		}
 	});
-
-	// Открытие и закрытие модального окна
 
 	const promoBtn = document.querySelector('.promo-btn');
 	const layer = document.querySelector('.layer');
@@ -52,18 +48,13 @@ document.addEventListener("DOMContentLoaded", function () {
 	function closeModal() {
 		layer.classList.remove('layer-open');
 		body.style.overflow = '';
-	} 
+	}
 
-	/* 	setTimeout(function () {
-			openModal();
-		}, 7000);
-	 */
-
-
-
+	setTimeout(function () {
+		openModal();
+	}, 7000);
 });
 
-// validation form
 
 const form = document.querySelector("form[name='contact-form']");
 const nameInput = document.querySelector("input[name='name']");
@@ -82,55 +73,55 @@ checkbox.isValid = () => checkbox.checked;
 const inputFields = [nameInput, phoneInput, checkbox];
 
 const isValidPhone = (phone) => {
-  const re = /[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-  return re.test(String(phone).toLowerCase());
+	const re = /[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+	return re.test(String(phone).toLowerCase());
 };
 
 let shouldValidate = false;
 let isFormValid = false;
 
 const validateInputs = () => {
-  console.log("we are here");
-  if (!shouldValidate) return;
+	console.log("we are here");
+	if (!shouldValidate) return;
 
-  isFormValid = true;
-  inputFields.forEach((input) => {
-    input.classList.remove("invalid");
-    input.nextElementSibling.classList.add("hide");
+	isFormValid = true;
+	inputFields.forEach((input) => {
+		input.classList.remove("invalid");
+		input.nextElementSibling.classList.add("hide");
 
-    if (!input.isValid()) {
-      input.classList.add("invalid");
-      isFormValid = false;
-      input.nextElementSibling.classList.remove("hide");
-    }
-  });
+		if (!input.isValid()) {
+			input.classList.add("invalid");
+			isFormValid = false;
+			input.nextElementSibling.classList.remove("hide");
+		}
+	});
 
-  if (!phoneInput.isValid()) {
-    phoneBox.classList.add("invalid");
-  } else {
-    phoneBox.classList.remove("invalid");
+	if (!phoneInput.isValid()) {
+		phoneBox.classList.add("invalid");
+	} else {
+		phoneBox.classList.remove("invalid");
 	}
-	
-  if (!checkbox.isValid()) {
-	  privacy.classList.add("invalid-checkbox");
-	  error.classList.remove("hide");
-  } else {
-	  privacy.classList.remove("invalid-checkbox");
-	  containerItem.classList.remove("hide");
-	  error.classList.add("hide");
+
+	if (!checkbox.isValid()) {
+		privacy.classList.add("invalid-checkbox");
+		error.classList.remove("hide");
+	} else {
+		privacy.classList.remove("invalid-checkbox");
+		containerItem.classList.remove("hide");
+		error.classList.add("hide");
 
 	}
 };
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  shouldValidate = true;
-  validateInputs();
-  if (isFormValid) {
-	  console.log("happy");
-	  form.reset();
-    // TODO: DO AJAX REQUEST
-  }
+	e.preventDefault();
+	shouldValidate = true;
+	validateInputs();
+	if (isFormValid) {
+		console.log("happy");
+		form.reset();
+		// TODO: DO AJAX REQUEST
+	}
 });
 
 inputFields.forEach((input) => input.addEventListener("input", validateInputs));
@@ -141,7 +132,40 @@ $(document).ready(function () {
 		slidesToShow: 4,
 		slidesToScroll: 1,
 		prevArrow: '<button type="button" class="slick-prev"><img class="slick-arrow-img" src="../resources/icons/prev-slick.png" alt="button prev slider" /></button>',
-		nextArrow: '<button type="button" class="slick-next"><img class="slick-arrow-img" src="../resources/icons/next-slick.png" alt="button next slider" /></button>'
+		nextArrow: '<button type="button" class="slick-next"><img class="slick-arrow-img" src="../resources/icons/next-slick.png" alt="button next slider" /></button>',
+		responsive: [
+			{
+				breakpoint: 1440,
+				settings: {
+					arrows: true,
+					slidesToScroll: 1,
+					slidesToShow: 4
+
+				}
+			},
+			{
+				breakpoint: 1120,
+				settings: {
+					arrows: false
+
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToScroll: 1,
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					slidesToScroll: 1,
+					slidesToShow: 1
+				}
+			}
+		]
 	});
 });
 
